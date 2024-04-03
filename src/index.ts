b74ccb0227c19c66f68d16b4affe082b571ec445
Emanuel Suriano <emanuel.suriano@gmail.com>
2024-04-03 18:53:17 +0800

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,58 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Router,  RequestLike } from 'itty-router'
+
+export const router = Router({ base: '/api' })
 
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
+	kv: KVNamespace;
 }
 
+async function getViews(kv: KVNamespace, slug: string): Promise<number> {
+	const value = await kv.get<number>(slug, 'json')
+
+	return value === null ? 1 : value
+}
+
+async function setViews(kv: KVNamespace, slug: string, count: number) {
+	return kv.put(slug, JSON.stringify(count))
+}
+
+router.get('/:slug', async (req: RequestLike, env: Env): Promise<Response> => {
+	const { slug } = req.params ?? {}
+
+	if (!slug) {
+		return new Response('Missing slug', { status: 400 })
+	}
+
+	const count = await getViews(env.kv, slug)
+
+	return new Response(`${count}`, { status: 200 })
+})
+
+router.post('/:slug', async (req: RequestLike, env: Env): Promise<Response> => {
+	const { slug } = req.params ?? {}
+
+	if (!slug) {
+		return new Response('Missing slug', { status: 400 })
+	}
+
+	const count = await getViews(env.kv, slug)
+	let result = 1
+
+	if (count !== null) {
+		result = count + 1
+	}
+
+	setViews(env.kv, slug, result)
+
+	return new Response(`${result}`, { status: 200 })
+})
+
+router.all(
+	'*',
+	(req) =>
+		new Response(`API ${req.method} ${new URL(req.url).pathname} | ${new Date()}`, { status: 418 })
+)
+
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
-	},
+	fetch: router.fetch
 };
