505cbafc5ae84f8c59490e4d347cbb0f5ca069d1
Emanuel Suriano <emanuel.suriano@gmail.com>
2024-04-04 19:58:40 +0800

    fix kv storage

--- src/index.ts
+++ src/index.ts
@@ -1,58 +1,46 @@
-import { Router,  RequestLike } from 'itty-router'
+import { Router } from 'itty-router';
 
-export const router = Router({ base: '/api' })
+export const router = Router({ base: '/api' });
 
 export interface Env {
-	kv: KVNamespace;
-}
+	kv: KVNamespace; 
+} 
 
-async function getViews(kv: KVNamespace, slug: string): Promise<number> {
-	const value = await kv.get<number>(slug, 'json')
-
-	return value === null ? 1 : value
-}
-
-async function setViews(kv: KVNamespace, slug: string, count: number) {
-	return kv.put(slug, JSON.stringify(count))
-}
-
-router.get('/:slug', async (req: RequestLike, env: Env): Promise<Response> => {
-	const { slug } = req.params ?? {}
+router.get('/:slug', async (req, env: Env) => {
+	const { slug } = req.params ?? {};
 
 	if (!slug) {
-		return new Response('Missing slug', { status: 400 })
+		return new Response('Missing slug', { status: 400 });
 	}
 
-	const count = await getViews(env.kv, slug)
+	try {
+		const count = (await env.kv.get<number>(slug)) || 1;
+		return new Response(`${count}`, { status: 200 });
+	} catch (err) {
+		console.error(`KV returned error: ${err}`);
+		return new Response(err as string, { status: 500 });
+	}
+});
 
-	return new Response(`${count}`, { status: 200 })
-})
-
-router.post('/:slug', async (req: RequestLike, env: Env): Promise<Response> => {
-	const { slug } = req.params ?? {}
+router.post('/:slug', async (req, env: Env) => {
+	const { slug } = req.params ?? {};
 
 	if (!slug) {
-		return new Response('Missing slug', { status: 400 })
+		return new Response('Missing slug', { status: 400 });
 	}
 
-	const count = await getViews(env.kv, slug)
-	let result = 1
-
-	if (count !== null) {
-		result = count + 1
+	try {
+		const count = Number(await env.kv.get(slug)) || 1;
+		await env.kv.put(slug, `${count + 1}`);
+		return new Response(`${count + 1}`, { status: 200 });
+	} catch (err) {
+		console.error(`KV returned error: ${err}`);
+		return new Response(err as string, { status: 500 });
 	}
+});
 
-	setViews(env.kv, slug, result)
-
-	return new Response(`${result}`, { status: 200 })
-})
-
-router.all(
-	'*',
-	(req) =>
-		new Response(`API ${req.method} ${new URL(req.url).pathname} | ${new Date()}`, { status: 418 })
-)
+router.all('*', (req) => new Response(`API ${req.method} ${new URL(req.url).pathname} | ${new Date()}`, { status: 418 }));
 
 export default {
-	fetch: router.fetch
+	fetch: router.fetch,
 };
